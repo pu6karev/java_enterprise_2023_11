b97dc052777a15df9eb388ecf0e0efbd878c5428
Pushkarev Vasiliy <pu6karev@gmail.com>
2024-01-12 15:54:09 +0200

    fixing contract_base path, add ComponentScan for test running

--- bank-manager-server/src/main/java/com/hillel/multi/Application.java
+++ bank-manager-server/src/main/java/com/hillel/multi/Application.java
@@ -2,8 +2,10 @@ package com.hillel.multi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.hillel.multi.controller", "com.hillel.multi.service"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
