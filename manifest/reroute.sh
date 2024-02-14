dc69d46079fe41438e35e5bb7fa0758e5bf9ae76
Pushkarev Vasiliy <pu6karev@gmail.com>
2024-02-14 18:38:47 +0200

    fix comment to english

--- manifest/reroute.sh
+++ manifest/reroute.sh
@@ -4,4 +4,4 @@ POD_NAME=$(kubectl get pods -l app=bank-manager -o jsonpath='{.items[0].metadata
 
 kubectl port-forward $POD_NAME 8080:8080
 
-echo "** Доступ к приложению по адресу http://localhost:8080 **"
+echo "** Access to the application at http://localhost:8080 **"
